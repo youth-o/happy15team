8785ce90be952b00c7544acc5b1c512ea6964746
seungcar <ysjysj777@naver.com>
2024-04-16 14:54:37 +0900

    feat: 나의대시보드 목록 그리드로 나눔

--- src/components/GridDashboardList/GridDashboardList.tsx
+++ src/components/GridDashboardList/GridDashboardList.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import styles from "./GridDashboardList.module.css";
 
+interface ListDatas {
+  text: string;
+  color: string;
+  crown: boolean;
+}
+
 const DashboardList = () => {
-  return <div className={styles.listContainer}>
+  const listDatas = [1,2,3,5,6,7,8,8];
+
+  return (
     <div>
-      대시보드 리스트
+      <div className={styles.gridContainer}>
+      <div className={styles.gridItem}>새로운 대시보드</div>
+        {listDatas.map((item, index) => (
+          <div key={index} className={styles.gridItem}>
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="page">
+        <button>
+          이전
+        </button>
+        <button>
+          다음
+        </button>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default DashboardList;
