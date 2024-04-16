764239cdb95af5b682a94f6f42cf6c6ea2001c83
seungcar <ysjysj777@naver.com>
2024-04-17 08:47:13 +0900

    feat: 인덱스 파일에 귀여운 이미지 추가(임시)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <div>
-      홈
+      <img
+        src="https://img.freepik.com/free-photo/cute-ai-generated-cartoon-bunny_23-2150288870.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1713225600&semt=ais"
+        width={500}
+      />
+      <div style={{fontSize: 50, color: 'pink'}}>완성까지 화이팅합시다!!!!</div>
     </div>
   );
 };
