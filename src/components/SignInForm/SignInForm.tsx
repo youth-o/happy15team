9c1c0e272fefcd9f6e06c9720daa41bf8f29a031
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-04-16 19:10:33 +0900

    feat: 로그인 페이지 기능 구현 및 토글

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,75 +1,108 @@
 import { useState } from "react";
-import eyeOn from "../../../public/images/eyeOn.svg";
-import eyeOff from "../../../public/images/eyeOff.svg";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import axios from "axios";
+import styles from "./SignInForm.module.css";
+import Image from "next/image";
+import { LoginData } from "@/types/interface";
 
-export default function SignIn() {
-  const [showEmailError, setShowEmailError] = useState<string | null>(null); //이메일 입력 에러 처리
-  const [showPasswordError, setShowPasswordError] = useState<string | null>(
-    null
-  ); //비밀번호 입력 에러 처리
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [showPassword, setShowPassword] = useState(false); //비밀번호 eye버튼 상태
+const formSchema = yup.object({
+  email: yup
+    .string()
+    .required("이메일을 입력해 주세요.")
+    .email("이메일 형식으로 작성해 주세요."),
+  password: yup
+    .string()
+    .required("8자 이상 입력해 주세요.")
+    .min(8, "8자 이상 입력해 주세요.")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    ),
+});
 
-  const emailError = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (e.target.validity.typeMismatch) {
-      setShowEmailError("이메일 형식으로 작성해 주세요.");
-      return;
-    }
-    setShowEmailError("");
+function SignInForm() {
+  const [seePassword, setSeePassword] = useState<boolean>(false);
+
+  const seePasswordHandler = () => {
+    setSeePassword(!seePassword);
   };
 
-  const passwordError = (e: React.FocusEvent<HTMLInputElement>) => {
-    if (e.target.validity.tooShort) {
-      setShowPasswordError("8자 이상 작성해 주세요.");
-      return;
-    }
-    setShowPasswordError("");
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    mode: "onBlur",
+    resolver: yupResolver(formSchema),
+  });
 
-  const handleEyeButton = () => {
-    setShowPassword(!showPassword);
+  const onSubmit = async (data: LoginData) => {
+    try {
+      await axios.post("/users", data);
+      console.log("회원가입 성공:", data);
+    } catch (error) {
+      console.error("회원가입 실패:", error);
+    }
   };
 
   return (
-    <div>
-      <label htmlFor="email" className="emailText">
-        이메일
-      </label>
-      <input
-        id="email"
-        type="email"
-        className="emailInput"
-        //추후에 이메일 입력 에러 텍스트 클래스 추가 필요
-        onBlur={emailError}
-        placeholder="이메일을 입력해 주세요"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      {showEmailError && <div className="emailErrorText">{showEmailError}</div>}
-      <label htmlFor="password" className="passwordText">
-        비밀번호
-        <button type="button" className="eyeButton" onClick={handleEyeButton}>
-          {!showPassword ? (
-            <img src={eyeOn} alt="eyeOn" />
-          ) : (
-            <img src={eyeOff} alt="eyeOff" />
-          )}
-        </button>
-      </label>
-      <input
-        id="password"
-        type={showPassword ? "text" : "password"}
-        className="passwordInput"
-        //추후에 비밀번호 입력 에러 텍스트 클래스 추가 필요
-        onBlur={passwordError}
-        minLength={8}
-        placeholder="비밀번호를 입력해 주세요."
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      {showPasswordError && (
-        <div className="passwordErrorText">{showPasswordError}</div>
-      )}
-      <button>로그인</button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="email" className={styles.emailText}>
+          이메일
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="이메일을 입력해 주세요"
+          {...register("email")}
+          className={errors.email ? styles.errorFocus : styles.notError}
+        />
+        {errors.email && (
+          <div className={styles.error}>{errors.email.message}</div>
+        )}
+      </div>
+      <div>
+        <label htmlFor="password" className={styles.passwordText}>
+          비밀번호
+          <button
+            type="button"
+            className="eyeButton"
+            onClick={seePasswordHandler}
+          >
+            {!seePassword ? (
+              <Image
+                src="/images/eyeOn.svg"
+                width={15}
+                height={15}
+                alt="eyeOn"
+              />
+            ) : (
+              <Image
+                src="/images/eyeOff.svg"
+                width={15}
+                height={15}
+                alt="eyeOff"
+              />
+            )}
+          </button>
+        </label>
+        <input
+          id="password"
+          type={seePassword ? "text" : "password"}
+          placeholder="비밀번호를 입력해 주세요"
+          {...register("password")}
+          className={errors.password ? styles.errorFocus : styles.notError}
+        />
+        {errors.password && (
+          <div className={styles.error}>{errors.password.message}</div>
+        )}
+      </div>
+      <button type="submit">로그인</button>
+    </form>
   );
 }
+
+export default SignInForm;
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,10 +1,12 @@
 import SignInForm from "@/components/SignInForm/SignInForm";
 import Link from "next/link";
+import SignInHeader from "@/components/SignInHeader/SignInHeader";
 
 function SignIn() {
   return (
     <div>
       <title>Taskify 로그인</title>
+      <SignInHeader />
       <SignInForm />
       <div>
         회원이 아니신가요?
