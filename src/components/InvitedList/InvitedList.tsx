452f9c6fef6208db85ac49eab4f9ae4297e2f936
seungcar <ysjysj777@naver.com>
2024-04-17 21:15:35 +0900

    feat: InvintedList - 초대없을때 구현

--- src/components/InvitedList/InvitedList.tsx
+++ src/components/InvitedList/InvitedList.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import styles from "./InvitedList.module.css";
+import styles from "./invitedList.module.css";
+import Image from "next/image";
 
 const InvitedList: React.FC = () => {
   return (
-    <div>
-      <h1 className={styles.h1}>초대받은 리스트놓을 곳</h1>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>초대받은 대시보드</h1>
+      <div className={styles.iconContainer}>
+        <Image
+          src="/images/InviteIcon.svg"
+          alt="Invited Icon"
+          width={100}
+          height={100}
+        />
+        <div>아직 초대받은 대시보드가 없어요</div>
+      </div>
     </div>
   );
 };
