899ba8a267936d5cf36468e986f1af3b4dcb46a8
Youth-o <2021111495@dgu.ac.kr>
2024-04-18 00:49:04 +0900

    feat: mypage 페이지 프로필 이미지 업로드 기능 추가

--- src/components/MyPageProfileForm/MyPageProfileForm.tsx
+++ src/components/MyPageProfileForm/MyPageProfileForm.tsx
@@ -1,13 +1,29 @@
 import styles from "./MyPageProfileForm.module.css";
 import { useRef } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
 function ProfileForm() {
-  // useRef를 이용해 input태그에 접근한다.
   const imageInput = useRef<HTMLInputElement>(null!);
+  const [image, setImage] = useState<string | null>(null);
 
-  // 버튼클릭시 input태그에 클릭이벤트를 걸어준다.
-  const handleCickImageUpload = () => {
+  // 파일이 선택되었을 때 호출되는 함수
+  const handleFileChange = (e: React.ChangeEvent) => {
+    const file = imageInput.current?.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        const result = reader.result;
+        if (typeof result === "string") {
+          setImage(result);
+        }
+      };
+    }
+    console.log(image);
+  };
+
+  const handleClickImageUpload = () => {
     imageInput.current.click();
   };
 
@@ -15,17 +31,21 @@ function ProfileForm() {
     <form className={styles.form}>
       <div className={styles.profileLabel}>프로필</div>
       <div className={styles.inputContainer}>
-        <div className={styles.fileContainer}>
-          <Image
-            className={styles.fileAddImg}
-            src="/images/add.svg"
-            width={30}
-            height={30}
-            alt="추가아이콘"
-            onClick={handleCickImageUpload}
-          />
-          <input type="file" ref={imageInput} />
-        </div>
+        <Image
+          className={styles.fileAddImg}
+          src={image ? image : "/images/add.svg"}
+          width={182}
+          height={182}
+          alt="추가아이콘"
+          onClick={handleClickImageUpload}
+        />
+        <input
+          className={styles.fileInput}
+          type="file"
+          accept="image/*"
+          ref={imageInput}
+          onChange={handleFileChange}
+        />
         <div>
           <div className={styles.editContainer}>
             <label htmlFor="email">이메일</label>
