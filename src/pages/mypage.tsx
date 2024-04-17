7ee9a2e2c81729d2085a861da5c5a4bd124bc178
Youth-o <2021111495@dgu.ac.kr>
2024-04-17 21:15:51 +0900

    feat: mypage 페이지 ProfileForm 컴포넌트 UI 완성

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,15 +1,12 @@
 import ProfileForm from "@/components/MyPageProfileForm/MyPageProfileForm";
 import Nav from "@/components/Nav/Nav";
 import Sidebar from "@/components/Sidebar/Sidebar";
-import { useState } from "react";
 
 function MyPage() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <>
       <Nav />
-      <Sidebar setShowModal={setShowModal} />
+      <Sidebar />
       <ProfileForm />
     </>
   );
