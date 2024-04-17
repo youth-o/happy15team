8f5339b7c375e256aa055c2280eed46ebbf01249
seungcar <ysjysj777@naver.com>
2024-04-17 20:42:51 +0900

    feat: 초대받은 리스트 파일 생성

---
+++ src/components/InvitedList/InvitedList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./InvitedList.module.css";
+
+const InvitedList: React.FC = () => {
+  return (
+    <div>
+      <h1 className={styles.h1}>초대받은 리스트놓을 곳</h1>
+    </div>
+  );
+};
+
+export default InvitedList;
