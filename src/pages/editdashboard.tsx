f18e9b9cf44f6864073d19cfb98f999bb2e974ac
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-04-18 16:38:04 +0900

    feat: Nav, 사이드 바 추가

---
+++ src/pages/editdashboard.tsx
@@ -0,0 +1,13 @@
+import Nav from "@/components/Nav/Nav";
+import Sidebar from "@/components/Sidebar/Sidebar";
+
+function EditDashboardPage() {
+  return (
+    <>
+      <Nav />
+      <Sidebar />
+    </>
+  );
+}
+
+export default EditDashboardPage;
