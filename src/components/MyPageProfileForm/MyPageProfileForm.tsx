e8f542648adb81ea95a4095058b6e6dd727fdd5b
Youth-o <2021111495@dgu.ac.kr>
2024-04-18 17:36:11 +0900

    feat: mypage 페이지 이메일 불러오기 및 api 로직 분리

--- src/components/MyPageProfileForm/MyPageProfileForm.tsx
+++ src/components/MyPageProfileForm/MyPageProfileForm.tsx
@@ -1,11 +1,34 @@
 import styles from "./MyPageProfileForm.module.css";
 import { useRef } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import UserService from "@/api/UserService";
+import axios from "@/lib/axios";
 import Image from "next/image";
 
 function ProfileForm() {
   const imageInput = useRef<HTMLInputElement>(null!);
   const [image, setImage] = useState<string | null>(null);
+  const [email, setEmail] = useState<string>("");
+
+  useEffect(() => {
+    // 회원 정보를 가져와서 이메일 정보를 설정
+    const fetchUserData = async () => {
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        try {
+          const userData = await UserService.getUserData(token);
+          setEmail(userData.email);
+          // 사용자 정보를 처리
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      } else {
+        // 토큰이 없을 경우 로그인 페이지로 리디렉션 또는 인증 필요 메시지 표시
+      }
+    };
+
+    fetchUserData();
+  }, []);
 
   // 파일이 선택되었을 때 호출되는 함수
   const handleFileChange = (e: React.ChangeEvent) => {
@@ -25,6 +48,20 @@ function ProfileForm() {
 
   const handleClickImageUpload = () => {
     imageInput.current.click();
+  };
+
+  const handleProfileUpload = async () => {
+    if (image) {
+      try {
+        const formData = new FormData();
+        formData.append("image", image);
+        const response = await axios.post("/users/me/image", formData);
+        console.log("Image uploaded successfully!");
+        // 이미지 업로드 후 어떤 작업을 수행할지 추가
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    }
   };
 
   return (
@@ -50,7 +87,13 @@ function ProfileForm() {
         <div>
           <div className={styles.editContainer}>
             <label htmlFor="email">이메일</label>
-            <input type="text" id="email" />
+            <input
+              type="text"
+              id="email"
+              value={email}
+              readOnly
+              placeholder={email}
+            />
           </div>
           <div className={styles.editContainer}>
             <label htmlFor="nickname">닉네임</label>
@@ -58,7 +101,13 @@ function ProfileForm() {
           </div>
         </div>
       </div>
-      <button className={styles.formBtn}>저장</button>
+      <button
+        type="submit"
+        className={styles.formBtn}
+        onClick={handleProfileUpload}
+      >
+        저장
+      </button>
     </form>
   );
 }
