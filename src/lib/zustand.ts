7ba4ebe44c83502d156df31bbac660d9dcb0068d
SeungAAA <155128712+SeungAAA@users.noreply.github.com>
2024-04-22 10:58:49 +0900

    chore : zustand 변수명 변경

--- src/lib/zustand.ts
+++ src/lib/zustand.ts
@@ -1,6 +1,5 @@
 import { create } from "zustand";
 
-
 //Todo
 //모달 이름 case화 해서 페이지 최상단에 코드 한줄로 적용시키기
 export interface IsetModals {
@@ -16,13 +15,13 @@ const setModals = create((set) => ({
   emailExisted: false, // 이메일 중복 모달 상태
   registerSuccess: false, // 회원가입 성공 모달 상태
   openModal: () => set({ modalState: true }),
-  closeModal: () => set({ modalState: false }),
-  openPasswordMismatchModal: () => set({ modalState: true }),
-  closePasswordMismatchModal: () => set({ modalState: false }),
+  closeModal: () => set({ m odalState: false }),
+  openPasswordMismatchModal: () => set({ PasswordMismatch: true }),
+  closePasswordMismatchModal: () => set({ PasswordMismatch: false }),
   openEmailExistedModal: () => set({ emailExisted: true }),
   closeEmailExistedModal: () => set({ emailExisted: false }),
   openRegisterSuccessModal: () => set({ registerSuccess: true }),
   closeRegisterSuccessModal: () => set({ registerSuccess: false }),
 }));
 
-export default setModals
+export default setModals;
