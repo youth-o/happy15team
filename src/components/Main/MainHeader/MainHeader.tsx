969c4045e7347a215460c60ffdd6181c3176dae3
Youth-o <2021111495@dgu.ac.kr>
2024-04-24 00:54:19 +0900

    feat: MainSection 컴포넌트 추가

--- src/components/Main/MainHeader/MainHeader.tsx
+++ src/components/Main/MainHeader/MainHeader.tsx
@@ -4,27 +4,25 @@ import Image from "next/image";
 
 function MainHeader() {
   return (
-    <>
-      <div className={styles.nav}>
-        <Link href="/">
-          <Image
-            src="/images/smallLogo.svg"
-            className={styles.logo}
-            width={115}
-            height={35}
-            alt="로고이미지"
-          />
+    <div className={styles.nav}>
+      <Link href="/">
+        <Image
+          src="/images/smallLogo.svg"
+          className={styles.logo}
+          width={115}
+          height={35}
+          alt="로고이미지"
+        />
+      </Link>
+      <div>
+        <Link href="/signin" className={styles.link}>
+          로그인
         </Link>
-        <div>
-          <Link href="/signin" className={styles.link}>
-            로그인
-          </Link>
-          <Link href="/signup" className={`${styles.link} ${styles.signup}`}>
-            회원가입
-          </Link>
-        </div>
+        <Link href="/signup" className={`${styles.link} ${styles.signup}`}>
+          회원가입
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import MainHeader from "@/components/Main/MainHeader/MainHeader";
+import MainSection from "@/components/Main/MainSection/MainSection";
 import React from "react";
 
 const Home = () => {
   return (
     <>
       <MainHeader />
+      <MainSection />
     </>
   );
 };
