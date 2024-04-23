1fe4553db14244ccee8b596843392e5d184f7d09
Youth-o <2021111495@dgu.ac.kr>
2024-04-24 01:40:56 +0900

    feat: 랜딩 페이지 로그인 되어 있으면 /dashboard로 리다이렉트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import MainFooter from "@/components/Main/MainFooter/MainFooter";
 import MainHeader from "@/components/Main/MainHeader/MainHeader";
 import MainSection from "@/components/Main/MainSection/MainSection";
-import React from "react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken"); // 혹은 다른 저장소에서 토큰을 가져옵니다.
+    if (token) {
+      // 토큰이 있다면 대시보드 페이지로 이동 원래는 /dashboard/첫번째 대시보드 아이디로 이동해야하는데
+      // 일단 그냥 /dashboard 로 이동
+      router.push("/dashboard/");
+    }
+  }, [router]);
   return (
     <>
       <MainHeader />
